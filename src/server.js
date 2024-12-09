bd2870ecb8fb0949ffba1bc3208d629f250f1414
Denys Volodko <babyleejane@gmail.com>
2024-12-09 20:17:54 +0200

    fix

--- src/server.js
+++ src/server.js
@@ -47,7 +47,7 @@ export const startServer = () => {
     });
   });
 
-  app.get((req, res) => {
+  app.get("*", (req, res) => {
     res.status(404).json({
       message: `${req.url} not found`,
     });
